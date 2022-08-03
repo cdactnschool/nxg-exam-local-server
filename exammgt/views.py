6ab40f1a2af7585f70d398f3d29087e868bf1785
sujit maddal <sujitm@cdac.in>
2022-08-03 19:11:12 +0530

    updare exammgt/views.py

--- exammgt/views.py
+++ exammgt/views.py
@@ -1,6 +1,7 @@
 
 from django.shortcuts import render
 
+
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
